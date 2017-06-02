0b158aec2a35f20e2708b97111b5d8039f977e2f
Daisuke Fujita <dtanshi45@gmail.com>
2017-06-03 00:25:58 +0900

    Write test for ListJobs

--- kubernetes/client_test.go
+++ kubernetes/client_test.go
@@ -53,6 +53,10 @@ func TestDeleteJob(t *testing.T) {
 				t.Errorf("error should not be raised: %s", err)
 			}
 		} else {
+			if err == nil {
+				t.Errorf("error should be raised")
+			}
+
 			if !strings.Contains(err.Error(), testcase.errMessage) {
 				t.Errorf("error message does not contain %q; got: %q", testcase.errMessage, err.Error())
 			}
@@ -104,6 +108,78 @@ func TestDeletePod(t *testing.T) {
 				t.Errorf("error should not be raised: %s", err)
 			}
 		} else {
+			if err == nil {
+				t.Errorf("error should be raised")
+			}
+
+			if !strings.Contains(err.Error(), testcase.errMessage) {
+				t.Errorf("error message does not contain %q; got: %q", testcase.errMessage, err.Error())
+			}
+		}
+	}
+}
+
+func TestListJobs(t *testing.T) {
+	obj := &batchv1.JobList{
+		Items: []batchv1.Job{
+			batchv1.Job{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "job1",
+					Namespace: "namespace",
+				},
+			},
+			batchv1.Job{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "job2",
+					Namespace: "namespace",
+				},
+			},
+			batchv1.Job{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "job3",
+					Namespace: "namespace-foobar",
+				},
+			},
+		},
+	}
+	clientset := fake.NewSimpleClientset(obj)
+	client := &Client{
+		clientset: clientset,
+	}
+
+	testcases := []struct {
+		namespace  string
+		expected   int
+		errMessage string
+	}{
+		{
+			namespace:  "namespace",
+			expected:   2,
+			errMessage: "",
+		},
+		{
+			namespace:  "foobar",
+			expected:   0,
+			errMessage: "",
+		},
+	}
+
+	for _, testcase := range testcases {
+		jobs, err := client.ListJobs(testcase.namespace)
+
+		if testcase.errMessage == "" {
+			if err != nil {
+				t.Errorf("error should not be raised: %s", err)
+			}
+
+			if len(jobs.Items) != testcase.expected {
+				t.Errorf("the number of items does not match; expected: %d, got: %v", testcase.expected, jobs)
+			}
+		} else {
+			if err == nil {
+				t.Errorf("error should be raised")
+			}
+
 			if !strings.Contains(err.Error(), testcase.errMessage) {
 				t.Errorf("error message does not contain %q; got: %q", testcase.errMessage, err.Error())
 			}
